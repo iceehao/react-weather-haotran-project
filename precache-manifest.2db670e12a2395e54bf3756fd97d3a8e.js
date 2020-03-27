e7cbdb55f404a1b4e8431ebbd85b95d3e74db5ef
iceehao <icehao1999@gmail.com>
2020-03-27 14:13:42 -0700

    Updates

---
+++ precache-manifest.2db670e12a2395e54bf3756fd97d3a8e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4faecb168701a72924a0ec6f868f27",
+    "url": "/react-weather-haotran-project/index.html"
+  },
+  {
+    "revision": "fc5483f8d38a80b43a0e",
+    "url": "/react-weather-haotran-project/static/css/2.5ad77178.chunk.css"
+  },
+  {
+    "revision": "117a4744463114427fcc",
+    "url": "/react-weather-haotran-project/static/css/main.5b4f1cee.chunk.css"
+  },
+  {
+    "revision": "fc5483f8d38a80b43a0e",
+    "url": "/react-weather-haotran-project/static/js/2.216d8e9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-haotran-project/static/js/2.216d8e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117a4744463114427fcc",
+    "url": "/react-weather-haotran-project/static/js/main.37543118.chunk.js"
+  },
+  {
+    "revision": "1f143ec650b38a24743b",
+    "url": "/react-weather-haotran-project/static/js/runtime-main.40a65373.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-weather-haotran-project/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-weather-haotran-project/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-weather-haotran-project/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-weather-haotran-project/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-weather-haotran-project/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-weather-haotran-project/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
